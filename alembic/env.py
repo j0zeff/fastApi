769b54d1a86528e9c2f2b9372ecaa34b76cd8260
praktykant2 <praktykant2@CF1.local>
2024-07-16 12:54:06 +0200

    fixed import issues in env.py

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from models import Base
-from models import parameterType, productParameters
+from models import ParameterType, ProductParameters
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
