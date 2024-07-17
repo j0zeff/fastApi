ee4fe26f0e1bb215537951574fcd9be5e9e3b84c
praktykant2 <praktykant2@CF1.local>
2024-07-17 14:01:14 +0200

    login added, not ready yet

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException, Header
+from typing import Annotated, Union
+from fastapi import Body, FastAPI, Depends, Form, HTTPException, Header
 from sqlalchemy.orm import Session
 from DbContext import SessionLocal
-from models import ProductParameters, TokenModel
+from models import ProductParameters, TokenModel, Users
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-
 
 def get_db():
     db = SessionLocal()
@@ -12,7 +13,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
 async def verify_token(
     appCodeClient: str = Header(None),
@@ -24,6 +24,11 @@ async def verify_token(
 
     if not appCode or not token:
         raise HTTPException(status_code=401, detail="Unauthorized")
+
+async def verify_authorization_token(accessToken: str = Header(None), db: Session = Depends(get_db)):
+    token = db.query(Users).filter(Users.access_token == accessToken).first()
+    if not token:
+        raise HTTPException(status_code=400, detail="Failed authorization")
 
 
 @app.get("/get_production_parameter_by_code/{code}")
@@ -86,3 +91,53 @@ async def get_params_by_parent_code(
         }
         for param in params
     ]
+
+@app.get("/login", response_class=HTMLResponse)
+async def log_in():
+    html_content = """
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Product parameters</title>
+        <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+        <div class="login-container">
+            <h1 class="title">Log In</h1>
+            <form action="/login" method="POST">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required><br><br>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required><br><br>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.post('/login')
+async def log_in(username: str = Form(), password: str = Form(), db: Session = Depends(get_db)) -> Union[Users, None]:
+    user = db.query(Users).filter(Users.username == username).first()
+    if user and user.check_password(password):
+        token = user.create_access_token()
+        return {"access_token": token, "token_type": "bearer"}
+    else: 
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+    
+@app.post('/create_user')
+async def create_user(username: str = Form(), password: str = Form(), db: Session = Depends(get_db)) -> Union[Users, None]:
+    existingUser = db.query(Users).filter(Users.username == username).first()
+    if existingUser:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
+    new_user = Users(username=username)
+    new_user.set_password(password)
+    db.add(new_user)
+    db.commit()
+    db.refresh()
+
+    return{"message": "User created succsessfuly", "user_id": new_user.id}
--- models.py
+++ models.py
@@ -1,8 +1,13 @@
+import secrets
+import string
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+import bcrypt
+from pydantic_sqlalchemy import sqlalchemy_to_pydantic
 
 Base = declarative_base()
+
 
 
 class ParameterType(Base):
@@ -34,3 +39,21 @@ class TokenModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     token = Column(String, unique=True, nullable=False)
     appCode = Column(String, nullable=False, index=True)
+
+class Users(Base):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True) 
+    password_hash = Column(String, nullable=False)
+    access_token = Column(String, nullable=False)
+
+    def set_password(self, password: str):
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password: str):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
+    
+    def create_access_token(self):
+        alphabet = string.ascii_letters + string.digits
+        self.access_token = ''.join(secrets.choice(alphabet) for i in range(20))
